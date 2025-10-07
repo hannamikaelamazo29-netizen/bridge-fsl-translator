0d023379af3da406a0550109a89c8db99bf48cd7
hannamikaelamazo29-netizen <hannamikaelamazo29@gmail.com>
2025-10-07 16:49:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -298,4 +298,8 @@ def fsl_letter(filename):
 
 # -------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
